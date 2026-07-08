a9b9cecce3ef755a4b027780147d19081c27a9ac
asyiraafnufail <asyiraaf4@gmail.com>
2026-07-08 13:31:37 +0700

    Membuka Gembok Keamanan Model (Mass Assignment)

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Expense extends Model
 {
-    //
+    use HasFactory;
+
+    // Izinkan pengisian data massal untuk kolom-kolom ini
+    protected $fillable = [
+        'date',
+        'description',
+        'amount',
+    ];
 }
