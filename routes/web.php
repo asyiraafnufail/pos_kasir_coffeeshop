46333dce7919d2e95b33539337a63272a3194c3d
asyiraafnufail <asyiraaf4@gmail.com>
2026-07-08 13:51:21 +0700

    Mendaftarkan Rute

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,14 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\KasirController;
 use App\Http\Controllers\MenuController;
 use App\Http\Controllers\ExpenseController;
-use App\Http\Controllers\DashboardController;   
+use App\Http\Controllers\DashboardController;  
+use App\Http\Controllers\EmployeeController; 
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return redirect()->route('login'); // Langsung arahkan halaman depan ke login
+    return redirect()->route('login'); 
 });
 
 Route::get('/dashboard', [DashboardController::class, 'index'])
@@ -28,9 +29,16 @@ Route::middleware('auth')->group(function () {
     Route::put('/menu/{id}', [MenuController::class, 'update'])->name('menu.update');
     Route::delete('/menu/{id}', [MenuController::class, 'destroy'])->name('menu.destroy');
 
+    // --- Rute Admin: Manajemen Pengeluaran ---
     Route::get('/pengeluaran', [ExpenseController::class, 'index'])->name('expense.index');
     Route::post('/pengeluaran', [ExpenseController::class, 'store'])->name('expense.store');
     Route::delete('/pengeluaran/{id}', [ExpenseController::class, 'destroy'])->name('expense.destroy');
+
+    // --- Rute Admin: Manajemen Karyawan ---
+    Route::get('/karyawan', [EmployeeController::class, 'index'])->name('employee.index');
+    Route::post('/karyawan', [EmployeeController::class, 'store'])->name('employee.store');
+    Route::put('/karyawan/{id}', [EmployeeController::class, 'update'])->name('employee.update');
+    Route::delete('/karyawan/{id}', [EmployeeController::class, 'destroy'])->name('employee.destroy');
 
     // --- Rute Profile ---
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
