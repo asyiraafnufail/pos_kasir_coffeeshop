d7aec5b48bc6d2bfee119ec19a752db08fc2e671
asyiraafnufail <asyiraaf4@gmail.com>
2026-07-08 13:18:36 +0700

    Perbarui Model Menu

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Menu extends Model
 {
-    //
+    use HasFactory;
+
+    // Daftarkan kolom yang diizinkan untuk diisi dari form (Mass Assignment)
+    protected $fillable = [
+        'name',
+        'category_id',
+        'price',
+        'is_available',
+    ];
 }
